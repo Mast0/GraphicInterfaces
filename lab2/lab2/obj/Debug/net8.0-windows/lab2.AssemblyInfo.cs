ed58659017a278187ccaa26e954b81a3dcbbbeff
Mast0 <bogdanremuga@gmail.com>
2025-04-07 23:25:15 +0300

    Create UI of application

--- lab2/lab2/obj/Debug/net8.0-windows/lab2.AssemblyInfo.cs
+++ lab2/lab2/obj/Debug/net8.0-windows/lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f007a70dff6f11cc9e00d2383cdcecbbe91c9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
